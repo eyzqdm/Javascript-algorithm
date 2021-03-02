699dca2c912c30b5da073ffc43639eb790a9e662
郭庆 <31536879@qq.com>
2021-03-02 18:27:18 +0800

    数组交集

---
+++ map/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-02 18:02:27
+ * @LastEditTime: 2021-03-02 18:02:36
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \JavaScript-algorithm\map\index.js
+ */
+const map = new Map()
---
+++ map/数组交集-字典.js
@@ -0,0 +1,20 @@
+var intersection = function (nums1, nums2) {
+
+    const obj = new Map()
+    const res = []
+    nums1.forEach((item) => {
+       obj.set(item,true)
+    })
+    // sd
+    nums2.forEach((item)=> {
+        if(obj.has(item)) {
+            res.push(item)
+            obj.delete(item)
+        }
+    })
+
+    return res
+};
+
+
+console.log(intersection([1,2,3],[2,3,4]))
