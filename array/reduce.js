99012308a1b3dc41dee68f9e8b06d5b263bc0021
郭庆 <31536879@qq.com>
2021-03-18 14:59:32 +0800

    reduce

--- array/reduce.js
+++ array/reduce.js
@@ -1,23 +1,37 @@
-Array.prototype.myReduce = function (cb,init){
+Array.prototype.myReduce = function (cb, init) {
+  // 提供初始值 从0位置开始循环
+  // 不提供初始值 位置0作为初始值 从位置1开始循环
+  if (init) {
+    this.forEach((item, index) => {
+      init = cb(init, item, index, this);
+    });
+  } else {
+    init = this.shift();
+    this.forEach((item, index) => {
+      init = cb(init, item, index, this);
+    });
+  }
 
-  this.forEach((item,index) => {
-     if(init !== 'undeifined')
-     {
-         init = cb(init,item,index,this)
-     }
-     else {
-         init = cb(0,item,index,this)
-     }
-  })
+  return init;
+};
 
-  return init
-
+const arr = [1, 2, 3, 4, 4, 4];
+const arr1 = [1, 2, [3, 4], [[5, 6]]];
+const arr2 = [1, 2, 22, 2, 2, 2, 2, 3, 4, 4, 4, 5, 5, 2, 77, 8];
+const sum = arr.myReduce((pre, cur) => {
+  return pre + cur;
+});
+function merge(arr) {
+  return arr.myReduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? merge(cur) : cur);
+  }, []);
 }
 
-const arr = [1,2,3,4,4,4]
+const repeat = arr2.myReduce((pre, cur) => {
+  if (!pre.includes(cur)) return pre.concat(cur);
+  return pre;
+}, []);
 
-const sum = arr.myReduce((pre,cur) => {
-    return pre+cur
-},2) 
-
-console.log(sum) // 20
+console.log(sum); // 20
+console.log(merge(arr1)); //[ 1, 2, 3, 4, 5, 6 ]
+console.log(repeat);
