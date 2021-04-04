d1d3bb293fc23e7e1e3142493b75297b47f418ab
郭庆 <31536879@qq.com>
2021-04-04 17:29:41 +0800

    merge

--- array/merge.js
+++ array/merge.js
@@ -9,7 +9,6 @@
 */
 
 var merge = function (nums1, m, nums2, n) {
-    
   while (m > 0 && n > 0) {
     if (nums1[m - 1] > nums2[n - 1]) {
       nums1[m + n - 1] = nums1[m - 1];
@@ -19,13 +18,12 @@ var merge = function (nums1, m, nums2, n) {
       n--;
     }
   }
-if(n!==0){
-   
-    while(n){
-        nums1[n-1] = nums2[n-1]
-        n--
+  if (n !== 0) {
+    while (n) {
+      nums1[n - 1] = nums2[n - 1];
+      n--;
     }
-}
+  }
   return nums1;
 };
 
---
+++ 排序和搜索/merge.js
@@ -0,0 +1,27 @@
+/*
+两个核心点
+1 递归拆分数组
+2 合并有序数组 
+ */
+function merge(arr) {
+  let res = [];
+  if (arr.length === 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, arr.length);
+  const orderLeft = merge(left);
+  const orderRight = merge(right);
+  while (orderLeft.length && orderRight.length) {
+    if (orderLeft[0] < orderRight[0]) {
+      res.push(orderLeft.shift());
+    } else {
+      res.push(orderRight.shift());
+    }
+  }
+  // concat 不会改变现有数组
+  if (orderLeft.length) res = [...res, ...orderLeft];
+  if (orderRight.length) res = [...res, ...orderRight];
+  console.log(res);
+  return res;
+}
+merge([8, 1, 4, 2, 6, 7, 3]);
