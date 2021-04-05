247e3e6d3b3aa1e906746b45f3e77318a83d4954
郭庆 <31536879@qq.com>
2021-04-05 22:39:37 +0800

    quick

---
+++ 排序和搜索/quick.js
@@ -0,0 +1,13 @@
+function quick(arr) {
+  if (arr.length === 1 || arr.length === 0) return arr;
+  const left = [];
+  const right = [];
+  const base = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < base) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+  return [...quick(left), base, ...quick(right)];
+}
+
+console.log(quick([2, 1, 6, 4, 7, 3, 8, 9]));
