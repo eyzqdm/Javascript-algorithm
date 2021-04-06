a0955553276a47190a18780f3cc5f2323df8531a
郭庆 <31536879@qq.com>
2021-04-06 11:10:45 +0800

    add

--- 排序和搜索/square.js
+++ 排序和搜索/square.js
@@ -5,14 +5,16 @@
 function sort(arr) {
   let res = [];
   for (let i = 0; i < arr.length; i++) {
-    res[arr[i]] = arr[i];
+    res[arr[i]] ? res[arr[i]].push(arr[i]) : (res[arr[i]] = [arr[i]]);
   }
-  return res.reduce((pre, cur) => {
-    if (cur) {
-      pre.push(cur);
-    }
-    return pre;
-  }, []);
+  return res
+    .reduce((pre, cur) => {
+      if (cur) {
+        pre.push(cur);
+      }
+      return pre;
+    }, [])
+    .flat();
 }
 
-console.log(sort([4, 5, 2, 1, 7, 9, 11, 44]));
+console.log(sort([4, 5, 2, 2, 1, 7, 2, 2, 9, 11, 44]));
