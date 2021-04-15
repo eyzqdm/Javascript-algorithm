ec06773a0440316eb4dcabf9defaae23d0247dc6
郭庆 <31536879@qq.com>
2021-04-15 10:14:20 +0800

    add

---
+++ tree/100.相同的树.js
@@ -0,0 +1,6 @@
+var isSameTree = function (p, q) {
+    if (p === null || q === null) {
+        return p === null && q === null
+    }
+    return (p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
+};
