e29c3e8eee6aaa8b029183cacc84ed2c7392a8e7
郭庆 <31536879@qq.com>
2021-07-26 21:58:01 +0800

    test

--- testttt.js
+++ testttt.js
@@ -1,1 +1,8 @@
 console.log('asdadas')
+var fs = require('fs');
+fs.writeFile("./tttttest.txt", "hello world!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
